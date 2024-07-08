08d59bd8407129b6c7ecb4b46f78282542db2085
jashanVIRDI12 <jashanvidi12@gmail.com>
2024-07-08 20:41:51 +0530

    home page update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.css';
 
 export default function Home() {
+    const [isHovered, setIsHovered] = useState(false);
+    const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+    const handleMouseMove = (e) => {
+        if (isHovered) {
+            const { clientX, clientY } = e;
+            const centerX = window.innerWidth / 2;
+            const centerY = window.innerHeight / 2;
+            const x = (clientX - centerX) / centerX;
+            const y = (clientY - centerY) / centerY;
+            setMousePosition({ x, y });
+        }
+    };
+
+    const handleMouseEnter = () => setIsHovered(true);
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+        setMousePosition({ x: 0, y: 0 });
+    };
+
+    const transformStyle = isHovered
+        ? {
+            transform: `perspective(1000px) rotateX(${mousePosition.y * 20}deg) rotateY(${-mousePosition.x * 20}deg)`
+        }
+        : {};
+
     return (
         <div
-            className="flex flex-col items-center justify-center min-h-screen text-black"
+            className="flex flex-col items-center justify-center min-h-screen text-white"
             style={{
                 backgroundImage: 'url(https://images.hdqwalls.com/wallpapers/gradient-blur-minimalism-fh.jpg)',
                 backgroundSize: 'cover',
@@ -13,21 +39,27 @@ export default function Home() {
                 width: '100%',
                 height: '100%'
             }}
+            onMouseMove={handleMouseMove}
         >
-            <h1 className="heading-gradient">
-                INVOICE GEN
-            </h1>
-            <p className="paragraph">
-                Effortlessly generate professional invoices in just a few clicks
-            </p>
-            <a href="/invoice" className="btn-animate">
-                <span>Get Started</span>
-            </a>
+            <div
+                className="div-hover p-24 bg-white bg-opacity-5 rounded-lg shadow-lg text-center"
+                style={transformStyle}
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+            >
+                <h1 className="heading-gradient mb-4">
+                    INVOICE GEN
+                </h1>
+                <p className="paragraph">
+                    Effortlessly generate professional invoices in just a few clicks
+                </p>
+                <a href="/invoice" className="btn-animate">
+                    <span>Get Started</span>
+                </a>
+            </div>
         </div>
     );
 }
-
-
 
 
 
